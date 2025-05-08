d2d10704ad79f5a60f2d2d3a91d3dc64e50512f3
Lennart Schneider <lennart.sch@web.de>
2025-05-08 13:49:50 +0200

    ..

--- run_yahpo_competitors.R
+++ run_yahpo_competitors.R
@@ -94,7 +94,7 @@ if (YAHPO_BENCHMARK == "pure_numeric") {
     ),
     target_variable = rep(c("val_accuracy", "acc"), c(4L, 16L)),
     direction = rep("maximize", 20L),
-    budget = rep(c(126L, 250L, 90L, 110L, 134L, 170L, 267L), c(3L, 1L, 2L, 2L, 2L, 4L, 6L))
+    budget = rep(c(126L, 254L, 90L, 110L, 134L, 170L, 267L), c(3L, 1L, 2L, 2L, 2L, 4L, 6L))
   )
 }
 
