d45d74572e534b200418c60cb30178144fe64f9f
be-marc <marcbecker@posteo.de>
2025-10-20 11:46:04 +0200

    install

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ conda install eigen libxml2 pkg-config zlib libpng xz
 
 export PKG_CONFIG_PATH=/glade/work/marcbecker/conda-envs/mbo_config/lib/pkgconfig/
 export LD_LIBRARY_PATH=/glade/work/marcbecker/conda-envs/mbo_config/lib:$LD_LIBRARY_PATH
+
+# hyperqueue
+git clone https://github.com/hyperqueue/hyperqueue.git
+cd hyperqueue
+RUSTFLAGS="-C target-cpu=native" cargo build --release --no-default-features
+export PATH=/glade/work/marcbecker/hyperqueue/target/release:$PATH
