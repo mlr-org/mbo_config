a6eb44ee950c8fa9745c98b1081b041df5c5329f
be-marc <marcbecker@posteo.de>
2025-11-17 13:15:37 -0700

    plots

--- competitors_2/plots.R
+++ competitors_2/plots.R
@@ -9,7 +9,8 @@ walk(c("numeric", "mixed"), function(benchmark) {
   data_aggr = fread(sprintf("competitors_2/results/%s_aggr.csv", benchmark), colClasses = c("instance" = "character"))
   instances = fread(sprintf("common/%s_instances.csv", benchmark), colClasses = c("instance" = "character"))
   data_aggr = data_aggr[instances, on = c("scenario", "instance")]
-  runtimes = fread(sprintf("competitors_2/results/%s_runtimes.csv", benchmark), colClasses = c("dim" = "factor"))
+  runtimes = fread(sprintf("competitors_2/results/%s_runtimes.csv", benchmark))
+  runtimes[, dim := as.factor(dim)]
 
   # rename algorithms
   data[algorithm == "smac4hpo", algorithm := "SMAC4HPO"]
@@ -45,7 +46,7 @@ walk(c("numeric", "mixed"), function(benchmark) {
 
   # mean RSNS
   pdf(sprintf("competitors_2/results/%s_performance.pdf", benchmark), width = 10, height = 10)
-  ggplot(data, aes(x = fraction_budget, y = mean_meta_score, color = algorithm, fill = algorithm)) +
+  gg = ggplot(data, aes(x = fraction_budget, y = mean_meta_score, color = algorithm, fill = algorithm)) +
     geom_line() +
     geom_ribbon(aes(min = mean_meta_score - se_meta_score, max = mean_meta_score + se_meta_score), colour = NA, alpha = 0.3) +
     ylim(-1, 1.5) +
@@ -54,11 +55,12 @@ walk(c("numeric", "mixed"), function(benchmark) {
     scale_fill_manual(values = algo_colors) +
     theme_minimal(base_size = 22) +
     theme(legend.position = "bottom")
+  print(gg)
   dev.off()
 
   # mean rank
   pdf(sprintf("competitors_2/results/%s_rank.pdf", benchmark), width = 10, height = 10)
-  ggplot(data, aes(x = fraction_budget, y = mean_rank, color = algorithm)) +
+  gg = ggplot(data, aes(x = fraction_budget, y = mean_rank, color = algorithm)) +
     geom_line() +
     geom_ribbon(aes(ymin = mean_rank - se_rank, ymax = mean_rank + se_rank, fill = algorithm), colour = NA, alpha = 0.2) +
     labs(x = "Fraction of Budget", y = "Mean Rank", color = "Algorithm", fill = "Algorithm") +
@@ -66,11 +68,12 @@ walk(c("numeric", "mixed"), function(benchmark) {
     scale_fill_manual(values = algo_colors) +
     theme_minimal(base_size = 22) +
     theme(legend.position = "bottom")
+  print(gg)
   dev.off()
 
   # per task RSNS
   pdf(sprintf("competitors_2/results/%s_performance_tasks.pdf", benchmark), width = 8, height = 6.5)
-  ggplot(data_aggr, aes(x = iter, y = meta_score, color = algorithm, fill = algorithm)) +
+  gg = ggplot(data_aggr, aes(x = iter, y = meta_score, color = algorithm, fill = algorithm)) +
     geom_line() +
     facet_wrap(~scenario + instance + name + dim, scales = "free_x", labeller = function(x) pmap_dtr(x, function(scenario, instance, name, dim) data.table(label_1 = paste0(scenario, " ", instance), label_2 = paste0(name, ", ", dim, "D")))) +
     ylim(-1, 1.5) +
@@ -78,16 +81,18 @@ walk(c("numeric", "mixed"), function(benchmark) {
     scale_color_manual(values = algo_colors) +
     scale_fill_manual(values = algo_colors) +
     theme_minimal(base_size = 12)
+  print(gg)
   dev.off()
 
   # runtimes
   pdf(sprintf("competitors_2/results/%s_runtimes.pdf", benchmark), width = 10, height = 10)
-  ggplot(mean_runtimes, aes(x = dim, y = mean_runtime, fill = algorithm)) +
+  gg = ggplot(mean_runtimes, aes(x = dim, y = mean_runtime, fill = algorithm)) +
     geom_bar(position = "dodge", stat = "identity") +
     labs(x = "Search Space Dimension", y = "Mean Runtime in Minutes", fill = "Algorithm") +
     scale_fill_manual(values = algo_colors) +
     theme_minimal(base_size = 22) +
     theme(legend.position = "bottom")
+  print(gg)
   dev.off()
 
   # critical difference 
