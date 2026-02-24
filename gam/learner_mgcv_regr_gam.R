0eb56541601693daf529c4847282df4f46334feb
be-marc <marcbecker@posteo.de>
2026-02-24 13:31:32 +0100

    add gam benchmarks

---
+++ gam/learner_mgcv_regr_gam.R
@@ -0,0 +1,153 @@
+#' @title Regression Generalized Additive Model Learner
+#' @author pierrecamilleri
+#' @name mlr_learners_regr.gam
+#'
+#' @description
+#' Generalized additive models. Calls `mgcv::gam()` from package \CRANpkg{mgcv}.
+#'
+#' @section Formula:
+#' A gam formula specific to the task at hand is required for the `formula`
+#' parameter (see example and `?mgcv::formula.gam`). Beware, if no formula is provided, a fallback formula is
+#' used that will make the gam behave like a glm (this behavior is required
+#' for the unit tests). Only features specified in the formula will be used,
+#' superseding columns with col_roles "feature" in the task.
+#'
+#' @section Offset:
+#' If a `Task` contains a column with the `offset` role, it is automatically
+#' incorporated during training via the `offset` argument in `mgcv::gam()`.
+#' No offset is applied during prediction for this learner.
+#'
+#' @template learner
+#' @templateVar id regr.gam
+#'
+#' @references
+#' `r format_bib("hastie2017generalized", "wood2012mgcv")`
+#'
+#' @template example_gam
+#' @export
+LearnerRegrGam = R6Class("LearnerRegrGam",
+  inherit = LearnerRegr,
+
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      param_set = ps(
+        block.size         = p_int(default = 1000L, tags = "predict"),
+        drop.intercept     = p_lgl(default = FALSE, tags = "train"),
+        drop.unused.levels = p_lgl(default = TRUE, tags = "train"),
+        edge.correct       = p_lgl(default = FALSE, tags = "train"),
+        efs.lspmax         = p_int(default = 15L, lower = 0L, tags = "train"),
+        efs.tol            = p_dbl(default = .1, lower = 0, tags = "train"),
+        epsilon            = p_dbl(default = 1e-07, lower = 0, tags = "train"),
+        family             = p_fct(default = "gaussian", levels = c("gaussian", "poisson"), tags = "train"),
+        formula            = p_uty(tags = "train"),
+        G                  = p_uty(default = NULL, tags = "train"),
+        gamma              = p_dbl(default = 1, lower = 1, tags = "train"),
+        H                  = p_uty(default = NULL, tags = "train"),
+        idLinksBases       = p_lgl(default = TRUE, tags = "train"),
+        in.out             = p_uty(default = NULL, tags = "train"),
+        irls.reg           = p_dbl(default = 0.0, lower = 0, tags = "train"),
+        knots              = p_uty(default = NULL, tags = "train"),
+        maxit              = p_int(default = 200L, tags = "train"),
+        method             = p_fct(
+          levels = c("GCV.Cp", "GACV.Cp", "REML", "P-REML", "ML", "P-ML"),
+          default = "GCV.Cp",
+          tags = "train"
+        ),
+        mgcv.half          = p_int(default = 15L, lower = 0L, tags = "train"),
+        mgcv.tol           = p_dbl(default = 1e-07, lower = 0, tags = "train"),
+        min.sp             = p_uty(default = NULL, tags = "train"),
+        ncv.threads        = p_int(default = 1, lower = 1, tags = "train"),
+        nei                = p_uty(tags = "train"),
+        newton             = p_uty(default = list(), tags = "train"),
+        nlm                = p_uty(default = list(), tags = "train"),
+        nthreads           = p_int(default = 1L, lower = 1L, tags = c("train", "threads")),
+        optim              = p_uty(default = list(), tags = "train"),
+        optimizer          = p_uty(default = c("outer", "newton"), tags = "train"),
+        paraPen            = p_uty(default = NULL, tags = "train"),
+        rank.tol           = p_dbl(default = .Machine$double.eps^0.5, lower = 0, tags = "train"),
+        scale              = p_dbl(default = 0, tags = "train"),
+        scale.est          = p_fct(levels = c("fletcher", "pearson", "deviance"), default = "fletcher", tags = "train"),
+        scalePenalty       = p_lgl(default = TRUE, tags = "train"),
+        select             = p_lgl(default = FALSE, tags = "train"),
+        sp                 = p_uty(default = NULL, tags = "train"),
+        trace              = p_lgl(default = FALSE, tags = "train"),
+        unconditional      = p_lgl(default = FALSE, tags = "predict")
+      )
+
+
+      super$initialize(
+        id = "regr.gam",
+        packages = c("mlr3extralearners", "mgcv"),
+        feature_types = c("logical", "integer", "numeric", "factor"),
+        predict_types = c("response", "se"),
+        param_set = param_set,
+        properties = c("weights", "offset"),
+        man = "mlr3extralearners::mlr_learners_regr.gam",
+        label = "Generalized Additive Regression Model"
+      )
+    }
+  ),
+
+  private = list(
+    .train = function(task) {
+      pars = self$param_set$get_values(tags = "train")
+      control_pars = pars[names(pars) %in% formalArgs(mgcv::gam.control)]
+      pars = pars[names(pars) %nin% formalArgs(mgcv::gam.control)]
+
+      data = task$data(cols = c(task$feature_names, task$target_names))
+      pars$weights = private$.get_weights(task)
+
+      if ("offset" %in% task$properties) {
+        pars$offset = task$offset$offset
+      }
+
+      if (is.null(pars$formula)) {
+        # GLM-like formula, no smooth terms
+        formula = stats::as.formula(paste(
+          task$target_names,
+          "~",
+          paste(task$feature_names, collapse = " + ")
+        ))
+        pars$formula = formula
+      }
+
+      control_obj = if (length(control_pars)) {
+        invoke(mgcv::gam.control, .args = control_pars)
+      } else {
+        mgcv::gam.control()
+      }
+
+      invoke(
+        mgcv::gam,
+        data = data,
+        .args = pars,
+        control = control_obj
+      )
+    },
+
+    .predict = function(task) {
+      pars = self$param_set$get_values(tags = "predict")
+      newdata = mlr3learners:::ordered_features(task, self)
+      include_se = (self$predict_type == "se")
+
+      preds = invoke(
+        predict,
+        self$model,
+        newdata = newdata,
+        type = "response",
+        newdata.guaranteed = TRUE,
+        se.fit = include_se,
+        .args = pars
+      )
+
+      if (include_se) {
+        list(response = preds$fit, se = preds$se)
+      } else {
+        list(response = preds)
+      }
+    }
+  )
+)
+
---
+++ gam/numeric_objective.R
@@ -0,0 +1,240 @@
+library(paradox)
+
+source("common/numeric_helper.R")
+
+numeric_objective = function(
+  scenario,
+  instance,
+  target_variable,
+  budget,
+  input_trafo,
+  output_trafo,
+  init,
+  init_size_fraction,
+  random_interleave_iter,
+  surrogate,
+  extratrees,
+  trees,
+  variance_estimator,
+  kernel,
+  nugget,
+  scaling,
+  acqf,
+  lambda,
+  acqopt,
+  epsilon_decay,
+  lambda_decay
+  ) {
+  assert_string(scenario)
+  assert_string(instance)
+  assert_string(target_variable)
+  assert_int(budget)
+
+  assert_string(input_trafo)
+  assert_string(output_trafo)
+  assert_string(init)
+  assert_number(init_size_fraction)
+  assert_int(random_interleave_iter)
+  assert_string(surrogate)
+  assert_flag(extratrees, na.ok = TRUE)
+  assert_int(trees, na.ok = TRUE)
+  assert_string(variance_estimator, na.ok = TRUE)
+  assert_string(kernel, na.ok = TRUE)
+  assert_number(nugget, na.ok = TRUE)
+  assert_flag(scaling, na.ok = TRUE)
+  assert_string(acqf)
+  assert_number(lambda, na.ok = TRUE)
+  assert_string(acqopt)
+  assert_flag(epsilon_decay, na.ok = TRUE)
+  assert_flag(lambda_decay, na.ok = TRUE)
+
+  reticulate::use_condaenv("yahpo_gym", required = TRUE) # /glade/work/marcbecker/conda-envs/yahpo_gym
+  data.table::setDTthreads(1L)
+  future::plan("sequential")
+
+  # create optim instance from yahpo gym problem
+  benchmark = BenchmarkSet$new(scenario, instance = instance)
+  benchmark$subset_codomain(target_variable)
+  objective = benchmark$get_objective(instance, multifidelity = FALSE)
+  search_space = get_search_space_pure_numeric(scenario)
+  objective = fix_objective_domain_constants_pure_numeric(scenario, objective = objective)
+  optim_instance = oi(objective, search_space = search_space, terminator = trm("evals", n_evals = budget))
+
+  # intial design
+  init_design_size = ceiling(init_size_fraction * budget)
+  init_design = if (init == "random") {
+    generate_design_random(optim_instance$search_space, n = init_design_size)$data
+  } else if (init == "lhs") {
+    generate_design_lhs(optim_instance$search_space, n = init_design_size)$data
+  } else if (init == "sobol") {
+    generate_design_sobol(optim_instance$search_space, n = init_design_size)$data
+  }
+  optim_instance$eval_batch(init_design)
+
+  # surrogate model
+  learner = if (surrogate == "rf") {
+    lrn("regr.ranger",
+      num.trees = trees,
+      se.method = variance_estimator,
+      splitrule = if (extratrees) "extratrees" else "variance",
+      predict_type = "se",
+      keep.inbag = TRUE,
+      sample.fraction = 1,
+      min.node.size = 3,
+      min.bucket = 3,
+      mtry.ratio = 5/6
+    )
+  } else if (surrogate == "gp") {
+    lrn("regr.km",
+      predict_type = "se",
+      control = list(trace = FALSE),
+      optim.method = "gen",
+      covtype = kernel,
+      nugget.stability = nugget,
+      scaling = scaling
+    )
+  } else if (surrogate == "gam") {
+    rhs = c(sprintf("s(%s, k=10)", search_space$ids()))
+    formula = reformulate(rhs, response = target_variable)
+    learner = LearnerRegrGam$new()
+    learner$configure(predict_type = "se", formula = formula)
+  }
+  surrogate = srlrn(learner)
+
+  # input trafo
+  if (input_trafo == "unitcube") {
+    surrogate$input_trafo = InputTrafoUnitcube$new()
+  }
+
+  # output trafo
+  if (output_trafo == "standardize") {
+    surrogate$output_trafo = OutputTrafoStandardize$new()
+  } else if (output_trafo == "log") {
+    surrogate$output_trafo = OutputTrafoLog$new(invert_posterior = FALSE)
+  }
+
+  # acq optimizer
+  dim = optim_instance$search_space$length
+  budget = 100L * dim^2
+
+  if (acqopt == "RS_1000") {
+    acq_optimizer = AcqOptimizerRandomSearch$new()
+    acq_optimizer$param_set$set_values(max_fevals = 1000L)
+  } else if (acqopt == "RS") {
+    acq_optimizer = AcqOptimizerRandomSearch$new()
+    acq_optimizer$param_set$set_values(max_fevals = budget)
+  } else if (acqopt == "LS") {
+    acq_optimizer = AcqOptimizerLocalSearch$new()
+    acq_optimizer$param_set$set_values(n_searches = 10L, n_steps = ceiling(budget / 300L), n_neighs = 30L)
+  } else if (acqopt == "DIRECT") {
+    acq_optimizer = AcqOptimizerDirect$new()
+    acq_optimizer$param_set$set_values(
+      maxeval = budget,
+      max_restarts = dim * 5L,
+      ftol_rel = 1e-4
+    )
+  } else if (acqopt == "CMAES") {
+    acq_optimizer = AcqOptimizerCmaes$new()
+    acq_optimizer$param_set$set_values(
+      max_fevals = budget,
+      max_restarts = 1000L
+
+    )
+  } else if (acqopt == "LBFGSB") {
+    acq_optimizer = AcqOptimizerLbfgsb$new()
+    acq_optimizer$param_set$set_values(
+      maxeval = budget,
+      max_restarts = dim * 5L,
+      ftol_rel = 1e-4
+    )
+  }
+
+  # acq function
+  acq_function = if (acqf == "EI" && output_trafo == "log") {
+    AcqFunctionEILog$new()
+  } else if (acqf == "EI" && output_trafo != "log") {
+    AcqFunctionEI$new()
+  } else if (acqf == "CB") {
+    AcqFunctionCB$new(lambda = lambda)
+  } else if (acqf == "PI") {
+    AcqFunctionPI$new()
+  } else if (acqf == "Mean") {
+    AcqFunctionMean$new()
+  }
+
+  if (isTRUE(epsilon_decay)) {
+    acq_function$constants$values$epsilon = 0.1
+    callback_decay_epsilon = callback_batch("mlr3mbo.decay_epsilon",
+      on_optimization_end = function(callback, context) {
+        epsilon = context$instance$objective$constants$get_values()[["epsilon"]]
+        context$instance$objective$constants$set_values("epsilon" = epsilon * 0.99)
+      }
+    )
+    acq_function$callbacks = list(callback_decay_epsilon)
+  }
+
+  if (isTRUE(lambda_decay)) {
+    callback_decay_lambda = callback_batch("mlr3mbo.decay_lambda",
+      on_optimization_end = function(callback, context) {
+        lambda = context$instance$objective$constants$get_values()[["lambda"]]
+        context$instance$objective$constants$set_values("lambda" = lambda * 0.99)
+      }
+    )
+    acq_function$callbacks = list(callback_decay_lambda)
+  }
+
+  bayesopt_ego(
+      optim_instance,
+      surrogate = surrogate,
+      acq_function = acq_function,
+      acq_optimizer = acq_optimizer,
+      random_interleave_iter = random_interleave_iter,
+      init_design_size = init_design_size)
+
+  optim_instance
+}
+
+if (FALSE) {
+  renv::load(".")
+
+  library(data.table)
+  library(mlr3)
+  library(mlr3learners)
+  library(mlr3misc)
+  library(mlr3mbo)
+  library(mlr3pipelines)
+  library(bbotk)
+  library(paradox)
+  library(R6)
+  library(checkmate)
+  library(yahpogym)
+
+  source("gam/learner_mgcv_regr_gam.R")
+  source("gam/numeric_objective.R")
+
+  xs = list(
+    scenario = "lcbench",
+    instance = "167168",
+    target_variable = "val_accuracy",
+    budget = 100L,
+    input_trafo = "none",
+    output_trafo = "none",
+    init = "random",
+    init_size_fraction = 0.05,
+    random_interleave_iter = 0L,
+    surrogate = "gam",
+    extratrees = FALSE,
+    trees = NA_integer_,
+    variance_estimator = NA_character_,
+    kernel = NA_character_,
+    nugget = NA_real_,
+    scaling = NA,
+    acqf = "EI",
+    lambda = NA_character_,
+    acqopt = "CMAES",
+    epsilon_decay = FALSE,
+    lambda_decay = FALSE
+  )
+
+  mlr3misc::invoke(numeric_objective, .args = xs)
+}
